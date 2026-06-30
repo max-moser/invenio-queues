d2984590e0f7b6f193d17f0702165db02f1fe5b6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 13:19:44 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2017-2020 CERN.
-# SPDX-FileCopyrightText: 2022 TU Wien.
-# SPDX-License-Identifier: MIT
-
-"""Invenio module for managing queues."""
-
-from setuptools import setup
-
-setup()
